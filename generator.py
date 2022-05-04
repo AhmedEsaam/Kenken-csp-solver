2936946506cfb783ceadba437c00cf1a6b487d7a
AhmedEssam <ahmed.e.mogy@gmail.com>
2022-05-04 14:15:20 +0200

    Adding cages creator

--- generator.py
+++ generator.py
@@ -1,6 +1,5 @@
 # Import the pygame module
-from this import d
-from xml import dom
+from msilib import change_sequence
 import pygame, sys, random
  
 # Import pygame.locals for easier access to key coordinates
@@ -10,7 +9,7 @@ from pygame.locals import *
 # Create the constants (go ahead and experiment with different values)
 BOARDWIDTH = 4  # number of columns in the board
 BOARDHEIGHT = 4 # number of rows in the board
-TILESIZE = 80
+TILESIZE = 70
 WINDOWWIDTH = 640
 WINDOWHEIGHT = 480
 FPS = 30
@@ -22,12 +21,16 @@ WHITE =         (255, 255, 255)
 BRIGHTBLUE =    (  0,  50, 255)
 DARKTURQUOISE = (  3,  54,  73)
 BLUE =          (  0,  50, 255)
-GREEN =         (  0, 128,   0)
+GREEN =         ( 92, 149,  92)
 RED =           (255,   0,   0)
+BROWN =         (232, 201, 126)
+OFFGREEN =      (157, 204, 145)
+
+
 BGCOLOR = DARKTURQUOISE
 TILECOLOR = WHITE
 TEXTCOLOR = BLACK
-BORDERCOLOR = BLACK
+BORDERCOLOR = GREEN
 BASICFONTSIZE = 20
 SECFONTSIZE = 13
 TEXT = GREEN
@@ -37,7 +40,8 @@ BUTTONTEXTCOLOR = BLACK
 MESSAGECOLOR = WHITE
 
 XMARGIN = int((WINDOWWIDTH - (TILESIZE * BOARDWIDTH + (BOARDWIDTH - 1))) / 2)
-YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
+# YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
+YMARGIN = 80
 
 UP = 'up'
 DOWN = 'down'
@@ -49,17 +53,19 @@ def main():
     global DISPLAYSURF, BASICFONT, SECFONT, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT, GAMESIZE
 
     pygame.init()
+    GAMESIZE = 4   # 3*3
+    WINDOWWIDTH = GAMESIZE * TILESIZE + 250
+    WINDOWHEIGHT = GAMESIZE * TILESIZE + 200
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     pygame.display.set_caption('Slide Puzzle')
     BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
     SECFONT = pygame.font.Font('freesansbold.ttf', SECFONTSIZE)
 
     # Store the option buttons and their rectangles in OPTIONS.
-    RESET_SURF, RESET_RECT = makeText('Reset',    TEXT, BGCOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 310)
-    NEW_SURF,   NEW_RECT   = makeText('New Game', TEXT, BGCOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 280)
-    SOLVE_SURF, SOLVE_RECT = makeText('Solve',    TEXT, BGCOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 250)
+    RESET_SURF, RESET_RECT = makeText('Reset',    TEXT, BGCOLOR, 20, 180)
+    NEW_SURF,   NEW_RECT   = makeText('New Game', TEXT, BGCOLOR, 20, 150)
+    SOLVE_SURF, SOLVE_RECT = makeText('Solve',    TEXT, BGCOLOR, 20, 120)
 
-    GAMESIZE = 3   # 3*3
     drawBoard(GAMESIZE)
 
     while True:
@@ -71,6 +77,41 @@ def gameGenerator(n):
     permutes = random.sample(range(1,n+1),n)
     return list(firstRow[i:]+firstRow[:i] for i in permutes)
 
+
+def cagesCreator(game):
+    cages=[]
+    positions=[]
+    for i in range(GAMESIZE):
+        for j in range(GAMESIZE):
+            positions.append([i+1,j+1])
+    while(len(positions) != 0):
+        cage=[]
+        seed = random.choice(positions)
+        cage.append(seed.copy())
+        positions.remove(seed)
+
+        cageSize = random.randint(2,4)
+        rand_num = random.randint(1,100)
+        if rand_num < 10: cageSize = 1
+
+        for i in range(cageSize-1):
+            x = seed[0]
+            y = seed[1]
+            if [x,y+1] in positions: # going right
+                seed = [x,y+1]
+            elif [x,y-1] in positions: # going left
+                seed = [x,y-1]
+            elif [x+1,y] in positions: # going dowwn
+                seed = [x+1,y]
+            elif [x-1,y] in positions: # going up
+                seed = [x-1,y]
+            else: break
+            cage.append(seed.copy())
+            positions.remove(seed)
+        cages.append(cage)
+        
+    return cages 
+    
 
 
 def getLeftTopOfTile(tileX, tileY):
@@ -88,14 +129,14 @@ def drawTile(tilex, tiley, game, adjx=0, adjy=0):
     textRect = textSurf.get_rect()
     textRect.center = left + int(TILESIZE / 2) + adjx, top + int(TILESIZE / 2) + adjy
     # constraint in the top left
-    # constSurf = SECFONT.render(str(number), True, TEXTCOLOR)
-    # constRect = constSurf.get_rect()
-    # constRect.center = left + 11 + adjx, top + 11 + adjy
+    constSurf = SECFONT.render(str(game[GAMESIZE-tilex-1][GAMESIZE-tiley-1]), True, TEXTCOLOR)
+    constRect = constSurf.get_rect()
+    constRect.center = left + 11 + adjx, top + 11 + adjy
 
     pygame.draw.line(DISPLAYSURF, BORDERCOLOR, (left, top),  (left, top + TILESIZE), 3)
 
     DISPLAYSURF.blit(textSurf, textRect)
-    # DISPLAYSURF.blit(constSurf, constRect)
+    DISPLAYSURF.blit(constSurf, constRect)
 
 def makeText(text, color, bgcolor, top, left):
     # create the Surface and Rect objects for some text.
@@ -107,10 +148,13 @@ def makeText(text, color, bgcolor, top, left):
 def drawBoard(n):
     # Generate random game
     game = gameGenerator(n)
-    for i in game:
-        print(i) 
+    print(game)
 
-    DISPLAYSURF.fill(BGCOLOR)
+    p = cagesCreator(n)
+    print(p)
+    
+
+    DISPLAYSURF.fill(OFFGREEN)
     # if message:
     #     textSurf, textRect = makeText(message, MESSAGECOLOR, BGCOLOR, 5, 5)
     #     DISPLAYSURF.blit(textSurf, textRect)
