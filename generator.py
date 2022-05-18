c30866184a08f5daeaa9cc39fc7d630a022ed571
AhmedEsaam <ahmed.e.mogy@gmail.com>
2022-05-18 16:08:50 +0200

    update

--- generator.py
+++ generator.py
@@ -3,6 +3,8 @@ import pygame, sys, random
  
 # Import pygame.locals for easier access to key coordinates
 from pygame.locals import *
+from new_game import generateNewGame
+from solving import solveGame
 
 # Create the constants (go ahead and experiment with different values)
 BOARDWIDTH = 4  # number of columns in the board
@@ -17,7 +19,7 @@ BLANK = None
 BLACK =         (  0,   0,   0)
 WHITE =         (255, 255, 255)
 DARKGREY =      ( 55,  58,  64)
-OFFGREY =        (238, 238, 238)
+OFFGREY =       (238, 238, 238)
 BRIGHTBLUE =    (  0,  50, 255)
 DARKTURQUOISE = (  3,  54,  73)
 BLUE =          (  0,  50, 255)
@@ -56,7 +58,7 @@ def main():
     global DISPLAYSURF, BASICFONT, SECFONT, SECFONT2, SECFONT3, FONT, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT, SIZE_SURF, SIZE_RECT, SOL_SURF, SOL_RECT, GAMESIZE
     global GAME, CAGES, CONSTRAINTS
     pygame.init()
-    GAMESIZE = 4  # n*n
+    GAMESIZE = 3  # n*n
     WINDOWWIDTH = GAMESIZE * TILESIZE + 250
     WINDOWHEIGHT = GAMESIZE * TILESIZE + 200
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
@@ -68,150 +70,11 @@ def main():
     FONT = BASICFONT
 
     drawGameOptions()
-    generateNewGame(GAMESIZE)
+    GAME, CAGES, CONSTRAINTS = generateNewGame(GAMESIZE)
     drawBoard(GAMESIZE, None, CAGES, CONSTRAINTS)
     
     while True:
         eventHandler()
-
-
-def gameGenerator(n):
-    firstRow = random.sample(range(1,n+1),n)
-    permutes = random.sample(range(1,n+1),n)
-    return list(firstRow[i:]+firstRow[:i] for i in permutes)
-
-
-def cagesCreator():
-    cages=[]
-    positions=[]
-    for i in range(1,GAMESIZE+1):
-        for j in range(1,GAMESIZE+1):
-            positions.append([i,j])
-    while(len(positions) != 0):
-        cage=[]
-        seed = random.choice(positions)
-        cage.append(seed.copy())
-        positions.remove(seed)
-        # determine how frequent ecah cage size occurs 
-        cageSize = random.randint(2,4)
-        # rand_num = random.randint(1,100)
-        # if rand_num < 50: cageSize = 4
-        # if rand_num < 96: cageSize = 3
-        # if rand_num < 55: cageSize = 2
-        # if rand_num < 2: cageSize = 1
-        
-        for i in range(cageSize-1):
-            x = seed[0]
-            y = seed[1]
-            if [x,y+1] in positions: # going right
-                seed = [x,y+1]
-            elif [x,y-1] in positions: # going left
-                seed = [x,y-1]
-            elif [x+1,y] in positions: # going dowwn
-                seed = [x+1,y]
-            elif [x-1,y] in positions: # going up
-                seed = [x-1,y]
-            else: break
-            cage.append(seed.copy())
-            positions.remove(seed)
-        cages.append(cage)
-
-    return cages
-
-def hasMultipleSol(cages):  # Check if current cages distribution has multiple solutions
-    # Check common columns and rows in cages 
-    cols=[]  # left column number of every side-by-side horizontally two tiles in the same cage
-    rows=[]  # upper row number of every side-by-side vertically two tiles in the same cage
-    n_sized_vertical_cages = 0  #    n stands for Game size
-    n_sized_horizontal_cages = 0  
-    for cage in cages:
-        same_col = 0
-        same_row = 0
-        for tile in cage:
-            h = tile[0]
-            v = tile[1]
-            if [h, v+1] in cage:
-                cols.append(v)
-                same_row += 1
-            if [h+1, v] in cage:
-                rows.append(h)
-                same_col += 1
-        if same_col == GAMESIZE-1: n_sized_vertical_cages += 1
-        if same_row == GAMESIZE-1: n_sized_horizontal_cages += 1
-
-    for i in range(1,GAMESIZE+1):
-        if (cols.count(i) == GAMESIZE) or (rows.count(i) == GAMESIZE):
-            return 1
-    # Check if there exist more than one vertical cages same size as game size,
-    #  the same goes for horizontal cages
-    if (n_sized_vertical_cages > 1) or (n_sized_horizontal_cages > 1):
-        return 1
-
-    return 0
-
-
-def constraintCreator(game,cages):
-    constraints =[]
-    for cage in cages:
-        constraint ={}
-        # determine topleft tile and values in the cage
-        values=[]
-        topleft_tile = cage[0]
-        for tile in cage:
-            values.append(game[tile[0]-1][tile[1]-1])
-            tiley = tile[0]; tilex = tile[1]
-            if tiley < topleft_tile[0] or (tiley == topleft_tile[0] and tilex <= topleft_tile[1]):
-                topleft_tile = tile
-        constraint['topleft'] = topleft_tile
-
-        # pick an arithmetic operation for the cage
-        cage_size = len(cage)
-        if cage_size == 1:
-            op = ' '
-        elif cage_size == 2 and max(values[0],values[1]) % min(values[0],values[1]) == 0: 
-            op = random.choice(['+','-','÷','x'])
-        elif cage_size == 2:
-            op = random.choice(['+','-','x'])
-        else :
-            op = random.choice(['+','x'])
-        constraint['op'] = op
-
-        # determine constraint value based on the operation
-        cnst = values[0]
-        if op=='+':
-            cnst = 0
-            for v in values: cnst += v 
-        elif op=='x':
-            cnst = 1
-            for v in values: cnst *= v 
-        elif op=='-':
-            cnst = abs(values[0]-values[1])
-        elif op=='÷':
-            cnst = int(max(values[0],values[1]) / min(values[0],values[1]))
-        constraint['constraint_value'] = cnst
-        constraints.append(constraint)
-    return constraints
-
-
-def generateNewGame(n):
-    global GAME, CAGES, CONSTRAINTS
-    # Generate random game
-    GAME = gameGenerator(n)
-    # print(game)
-    for i in GAME:
-        print(i)
-    # Create cages for the game
-    CAGES = cagesCreator()
-    while hasMultipleSol(CAGES):
-        CAGES = cagesCreator()
-    print(CAGES)
-    # Determine the constraints
-    CONSTRAINTS = constraintCreator(GAME, CAGES)
-    print(CONSTRAINTS)
-
-def solveGame(GAMESIZE, CAGES, CONSTRAINTS): # TO BE CHANGED TO A CSP SOLVER
-    return GAME   # TO BE CHANGED TO A CSP SOLVER
-
 
 
 # Game Draw___________________________________________________________________
@@ -367,12 +230,13 @@ class button():
 global val
 val = -1
 def eventHandler():
-    global HOVERTILE, PRESSEDTILE, TILES, TILESPOS, GAMEDISPLAYED
+    global GAME, CAGES, CONSTRAINTS, HOVERTILE, PRESSEDTILE, TILES, TILESPOS, GAMEDISPLAYED
     global GAMEDISPLAYED, val, PRESSEDTILE
     val = -1
     for event in pygame.event.get():
         ## get position of cursor
         pos = pygame.mouse.get_pos()
+
         # Handle mouse press events
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             ## check if cursor is on tile ##
@@ -384,12 +248,13 @@ def eventHandler():
             ## check if cursor is on button ##
             if NEW_RECT.collidepoint(pos):
                 # New game
-                generateNewGame(GAMESIZE)
+                GAME, CAGES, CONSTRAINTS = generateNewGame(GAMESIZE)
                 GAMEDISPLAYED = None
                 pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
             elif SOLVE_RECT.collidepoint(pos):
                 # Solve game
-                solved = solveGame(GAMESIZE, CAGES, CONSTRAINTS)
+                solved = GAME  # comment
+                # solved = solveGame(GAMESIZE, CAGES, CONSTRAINTS, 'BT')
                 GAMEDISPLAYED = solved
                 pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
             elif RESET_RECT.collidepoint(pos):
--- solving.py
+++ solving.py
@@ -1,27 +1,34 @@
 def solveGame(GAMESIZE, CAGES, CONSTRAINTS, technique): # TO BE CHANGED TO A CSP SOLVER
-    # GAMESIZE : n 
 
     '''
-    if the game size was (3*3) and solution was :
+    (GAMESIZE) : is the side size of the game
+        if the game size was (3*3) then GAMESIZE = 3, and solution might be :
+
         [2, 3, 1]
         [1, 2, 3]
         [3, 1, 2]
 
-    (CAGES) includes the (row,col) indices of squares in each cage.
+    (CAGES) : includes the (row,col) indices of squares in each cage.
         A CAGES distribution might be:
 
         [[[3, 1], [3, 2], [3, 3], [2, 3]],
          [[2, 2], [2, 1], [1, 1]],
          [[1, 3], [1, 2]]]
 
-    (CONSTRAINTS) includes the operation used in each cage (+,-,x,÷), the constraint value,
+    (CONSTRAINTS) : includes the operation used in each cage (+,-,x,÷), the constraint value,
         and the most top left square in each cage which is irrelevant in this function context
         - top left square becomes handy when displaying the board only- 
         an example of CONSTRAINTS might be:
-        
+
         [{'topleft': [2, 3], 'op': 'x', 'constraint_value': 18},
          {'topleft': [1, 1], 'op': 'x', 'constraint_value': 4},
          {'topleft': [1, 2], 'op': '+', 'constraint_value': 4}]
+
+    (technique) : is a string value of the solving technique used one of ('TB', 'FC', 'AC')
+
+        'TB' : Backtracking
+        'FC' : Backtracking with forward checking
+        'AC' : Backtracking with forward checking and arc consistency
     
     '''
 
