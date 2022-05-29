1ca98a4a7f68f405e78a18d61cc4df83783fe6a1
AhmedEsaam <ahmed.e.mogy@gmail.com>
2022-05-29 20:22:04 +0200

    run different techniques on the same board

--- analysis.py
+++ analysis.py
@@ -8,20 +8,26 @@ writer = writer(file, dialect='excel')
 writer.writerow(["Technique", "Size", "Assignments", "time_elapsed"])
 
 techniques = ['BT', 'FC', 'AC']
-MAX_SIZE = 5
-for technique in techniques:
-    for n in range(1, MAX_SIZE):
-        for i in range(4):
-            solved = 'failure'
-            while (solved == 'failure'):
-                # Generate new game
-                size = n+1
-                game, cages, constraints = generateNewGame(size)
-                # solve and record time elapsed
-                t = time()
-                solved, assignments = solveGame(size, cages, constraints, technique)
-                time_elapsed = time() - t
+heuristics = ['None', 'MCV']  # MCV: Most Constraining Variable
+MAX_SIZE = 4
 
-            writer.writerow([technique, size, assignments, time_elapsed])
+for n in range(1, MAX_SIZE):
+    for i in range(2):
+         # Generate new game
+        size = n+1
+        game, cages, constraints = generateNewGame(size)
+        for technique in techniques:
+            for heuristic in heuristics:
+                solved = 'failure'
+                while (solved == 'failure'):
+                    # solve and record time elapsed
+                    t = time()
+                    solved, assignments = solveGame(size, cages, constraints, technique, heuristic)
+                    time_elapsed = time() - t
+
+                Algorithm = technique
+                if heuristic != 'None':
+                    Algorithm = technique + ' with ' + heuristic
+                writer.writerow([Algorithm, size, assignments, time_elapsed])
 
 file.close()
